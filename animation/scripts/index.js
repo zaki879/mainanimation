458e6aa18fd47f7babf9fd04cf8232bfc4fa9f23
zaki879 <zaki.bouchebtoul@univ-constantine2.dz>
2024-08-26 18:38:10 +0100

    this.getScrollProps(e)

--- animation/scripts/index.js
+++ animation/scripts/index.js
@@ -678,7 +678,6 @@ var lr = l(() => {
 function g(i, t, e, r) {
   return (
     typeof t == "string" && (t = [t]),
-    t.forEach((s) => i.addEventListener(s, e, r)),
     () =>
       t.forEach((s) => {
         i.removeEventListener(s, e, r);
