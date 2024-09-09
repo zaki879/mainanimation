380bee60afa365e9e97b9370a587076608b75b50
zaki879 <zaki.bouchebtoul@univ-constantine2.dz>
2024-09-09 14:29:09 +0100

    try to fix2 the bundle3

--- animation/scripts/index.js
+++ animation/scripts/index.js
@@ -5899,7 +5899,7 @@ var zt,
             ],
           }),
         ],
-        zt
+        zt  
       ));
     xo = new zt();
   });
@@ -5907,10 +5907,10 @@ Lt();
 var Gi = !1;
 async function wo() {
   if (F()) {
-    ((Gi = !0), await Promise.resolve().then(() => (Kr(), Zr)))
+    await Promise.resolve().then(() => (us(), hs))
   }
     // ? 
-    // : await Promise.resolve().then(() => (us(), hs));
+    // : ;
 }
 wo();
 window.addEventListener("resize", () => {
